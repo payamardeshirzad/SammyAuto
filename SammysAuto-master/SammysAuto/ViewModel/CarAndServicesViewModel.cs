9e6e9dd457f198c0fd4b2e35f582ef035308e527
Payam Ardeshirzad <payam@ardeshirzad.com>
2020-04-22 09:20:18 +1200

    Sammy Auto Project

---
+++ SammysAuto-master/SammysAuto/ViewModel/CarAndServicesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SammysAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SammysAuto.ViewModel
+{
+    public class CarAndServicesViewModel
+    {
+
+        public int carId { get; set; }
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Style { get; set; }
+        public int Year { get; set; }
+        public string UserId { get; set; }
+
+        public Service NewServiceObj { get; set; }
+        public IEnumerable<Service> PastServicesObj { get; set; }
+        public List<ServiceType> ServiceTypesObj { get; set; }
+    }
+}
